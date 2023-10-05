4cab94dfd19dcd3f1f2e0ae6b3d528d74127170a
Ijordan4 <124106922+Ijordan4@users.noreply.github.com>
2023-10-05 02:18:54 -0400

    Update webpack.config.js

--- develop/client/webpack.config.js
+++ develop/client/webpack.config.js
@@ -1,77 +1,75 @@
-const MyHtmlWebpackPlugin = require("html-webpack-plugin");
-const MyWebpackPwaManifest = require("webpack-pwa-manifest");
-const MyPath = require("path");
-const { MyInjectManifest } = require("workbox-webpack-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const workboxPlugin = require("workbox-webpack-plugin");
+const WebpackPwaManifest = require("webpack-pwa-manifest");
+const path = require("path");
 
-module.exports = () => {
-  return {
-    mode: "development",
-    entry: {
-      myMain: "./src/js/index.js",
-      myInstall: "./src/js/install.js",
-      myDatabase: "./src/js/database.js",
-      myEditor: "./src/js/editor.js",
-      myHeader: "./src/js/header.js",
-    },
-    output: {
-      filename: "[name].bundle.js",
-      path: MyPath.resolve(__dirname, "dist"),
-    },
-    plugins: [
-      
-      new MyHtmlWebpackPlugin({
-        template: "./index.html",
-        title: "My JATE",
-      }),
+const entryPoints = {
+  myMain: "./src/js/index.js",
+  myInstall: "./src/js/install.js",
+  myDatabase: "./src/js/database.js",
+  myEditor: "./src/js/editor.js",
+  myHeader: "./src/js/header.js",
+};
 
-     
-      new MyInjectManifest({
-        swSrc: "./src-sw.js",
-        swDest: "src-sw.js",
-      }),
+const outputConfig = {
+  filename: "[name].bundle.js",
+  path: path.resolve(__dirname, "dist"),
+};
 
-    
-      new MyWebpackPwaManifest({
-        fingerprints: false,
-        inject: true,
-        name: "My Just Another Text Editor",
-        short_name: "My JATE",
-        description: "My just another text editor",
-        background_color: "#225ca3",
-        theme_color: "#225ca3",
-        start_url: "/",
-        publicPath: "./",
-        icons: [
-          {
-            src: MyPath.resolve("src/images/logo.png"),
-            sizes: [96, 128, 192, 256, 384, 512],
-            destination: MyPath.join("assets", "icons"),
-          },
-        ],
-      }),
-    ],
+module.exports = {
+  mode: "development",
+  entry: entryPoints,
+  output: outputConfig,
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./index.html",
+      title: "My JATE",
+    }),
 
-    module: {
-      rules: [
+    new workboxPlugin.InjectManifest({
+      swSrc: "./src-sw.js",
+      swDest: "src-sw.js",
+    }),
+
+    new WebpackPwaManifest({
+      fingerprints: false,
+      inject: true,
+      name: "My Just Another Text Editor",
+      short_name: "My JATE",
+      description: "My just another text editor",
+      background_color: "#225ca3",
+      theme_color: "#225ca3",
+      start_url: "/",
+      publicPath: "./",
+      icons: [
         {
-          test: /\.css$/i,
-          use: ["style-loader", "css-loader"],
-        },
-        {
-          test: /\.m?js$/,
-          exclude: /node_modules/,
-          use: {
-            loader: "babel-loader",
-            options: {
-              presets: ["@babel/preset-env"],
-              plugins: [
-                "@babel/plugin-proposal-object-rest-spread",
-                "@babel/transform-runtime",
-              ],
-            },
-          },
+          src: path.resolve("src/images/logo.png"),
+          sizes: [96, 128, 192, 256, 384, 512],
+          destination: path.join("assets", "icons"),
         },
       ],
-    },
-  };
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.m?js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"],
+            plugins: [
+              "@babel/plugin-proposal-object-rest-spread",
+              "@babel/transform-runtime",
+            ],
+          },
+        },
+      },
+    ],
+  },
 };
